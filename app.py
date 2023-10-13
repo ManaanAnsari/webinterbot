69db2ca12be7914b4580e45ca809861bd14db654
ManaanAnsari <manaan.web@gmail.com>
2023-10-13 09:39:21 +0400

    changed form

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ async def launch_web_ui(update: Update, callback: CallbackContext):
     ]
     await update.message.reply_text("Let's do this...", reply_markup=ReplyKeyboardMarkup(kb))
 
+async def web_app_data(update: Update, context: CallbackContext):
+    data = json.loads(update.message.web_app_data.data)
+    await update.message.reply_text("Your data was:")
+    for result in data:
+        await update.message.reply_text(f"{result['name']}: {result['value']}")
 
 if __name__ == '__main__':
     # when we run the script we want to first create the bot from the token:
@@ -19,7 +24,7 @@ if __name__ == '__main__':
 
     # and let's set a command listener for /start to trigger our Web UI
     application.add_handler(CommandHandler('start', launch_web_ui))
-
+    application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data))
     # and send the bot on its way!
     print(f"Your bot is listening! Navigate to http://t.me/{BOT_USERNAME} to interact with it!")
     application.run_polling()
